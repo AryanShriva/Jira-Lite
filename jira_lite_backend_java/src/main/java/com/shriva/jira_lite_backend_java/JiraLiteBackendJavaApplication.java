fe46800544560fc1aeabc9398c9fd9154cbaab97
Aryan Shriva <aryanshriva08@gmail.com>
2025-05-30 17:15:54 +0530

    new changes

---
+++ jira_lite_backend_java/src/main/java/com/shriva/jira_lite_backend_java/JiraLiteBackendJavaApplication.java
@@ -0,0 +1,13 @@
+package com.shriva.jira_lite_backend_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraLiteBackendJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiraLiteBackendJavaApplication.class, args);
+	}
+
+}
---
+++ jira_lite_backend_java/src/test/java/com/shriva/jira_lite_backend_java/JiraLiteBackendJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shriva.jira_lite_backend_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JiraLiteBackendJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
