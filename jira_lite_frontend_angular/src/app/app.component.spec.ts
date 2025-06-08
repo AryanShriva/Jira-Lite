b690b6fb524619670bc86c0f5f8dfc1b0e887669
Aryan Shriva <aryanshriva08@gmail.com>
2025-06-08 10:10:23 +0530

    jira-lite-frontend setup

--- jira_lite_frontend_angular/src/app/app.component.spec.ts
+++ jira_lite_frontend_angular/src/app/app.component.spec.ts
@@ -1,12 +1,10 @@
-import { provideZonelessChangeDetection } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
-import { App } from './app';
+import { App } from './app.component';
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [App],
-      providers: [provideZonelessChangeDetection()]
     }).compileComponents();
   });
 
@@ -20,6 +18,6 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, jira_lite_frontend_angular');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, jira-lite-frontend');
   });
 });
--- jira_lite_frontend_angular/src/app/app.component.ts
+++ jira_lite_frontend_angular/src/app/app.component.ts
@@ -4,9 +4,9 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
-export class App {
-  protected title = 'jira_lite_frontend_angular';
+export class AppComponent {
+  protected title = 'jira-lite-frontend';
 }
